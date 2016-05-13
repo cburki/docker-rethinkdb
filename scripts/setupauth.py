4d6349884365500cfc3366f10888dc38be179dba
Christophe Burki <christophe.burki@gmail.com>
2016-05-13 07:46:41 -0400

    Fixed admin auth setup

--- scripts/setupauth.py
+++ scripts/setupauth.py
@@ -4,7 +4,7 @@ import rethinkdb as r
 def main() :
 
     password = os.environ['RETHINKDB_ADMIN_PASSWORD']
-    print("Setting up rethinkdb admin with password : {}".format(auth_key))
+    print("Setting up rethinkdb admin with password : {}".format(password))
 
     conn = r.connect('localhost', 28015)
     r.db('rethinkdb').table('users').get('admin').update({'password' : password}).run(conn)
